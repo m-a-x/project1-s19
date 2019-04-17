dec0da84517de3f5f2e12cbf961f5986602c9364
Max <mwmattioli@gmail.com>
2019-04-17 07:39:45 -0400

    Update webserver/server.py

--- webserver/server.py
+++ webserver/server.py
@@ -177,7 +177,14 @@ def login():
 
 @app.route('/add_meme_to_list', methods=['POST'])
 def add_meme_to_list():
-    return
+    list_chosen = str(request.form['list_choice'])
+    lid_chosen = session['displayname_to_lid'][list_chosen]
+    
+    post_chosen = request.form['post_choice']
+    cmd = 'INSERT INTO favoriteslistsposts(lid, pid) VALUES (:list_id, :post_id)'
+    g.conn.execute(text(cmd), list_id=lid, list_id=lid_chosen, post_id=post_chosen)
+    
+    return redirect('/')
 
 @app.route('/create')
 def create():
